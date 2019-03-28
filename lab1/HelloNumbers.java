0058bf717430518793e8c0405e3a6fdd75ca61a7
r-k-jonynas <e0191908@u.nus.edu>
2019-03-28 12:56:01 +0200

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,11 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        int sum = x;
+        while (x < 10) {
+        	sum += x;
+        	System.out.println(sum);
+        	x = x+1;
+        }
+    }
+}
---
+++ lab1/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println(x+10);
+    }
+}
