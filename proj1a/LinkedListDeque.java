8ce8e73b7116a0b6b427c778e4b58f9d12587e8b
r-k-jonynas <e0191908@u.nus.edu>
2019-04-03 19:25:01 +0300

    First v of Proj 1A

---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,157 @@
+public class LinkedListDeque<T> {
+	private class GenericNode {
+		private GenericNode prev;
+		private T item;
+		private GenericNode next;
+
+		private GenericNode(GenericNode p, T i, GenericNode n) {
+			prev = p;
+			item = i;
+			next = n;
+		}
+
+		private T traverseGenericNode(int depth) { 
+			if (depth == 0) {
+				return this.item;
+			}
+			return (this.next).traverseGenericNode(depth - 1);
+		}
+
+	}
+
+	private GenericNode sentFront;
+	private GenericNode sentBack;;
+	private int size;
+
+	public LinkedListDeque() {
+		sentFront = new GenericNode(null, null, null);
+		sentBack = new GenericNode(sentFront, null, null);
+		sentFront.next = sentBack;
+	}
+
+	/** Creating a deep copy means that you create an
+	 entirely new LinkedListDeque, with the exact same
+	 items as other. However, they should be different
+	 objects, i.e. if you change other, the new LinkedListDeque
+	 you created should not change as well. */
+	public LinkedListDeque(LinkedListDeque other) {
+		GenericNode temp = other.sentFront;
+		GenericNode tempForCopy = new GenericNode(temp.prev, temp.item, null);
+		sentFront = tempForCopy;
+		while (temp != other.sentBack) {
+         	temp = temp.next;
+			tempForCopy.next = new GenericNode(tempForCopy, temp.item, null);
+			tempForCopy = tempForCopy.next;
+		}
+		sentBack = tempForCopy;
+		size = other.size;
+	}
+
+	/**  Adds an item of type T to the front of the deque.*/
+	public void addFirst(T item) {
+		sentFront.next = new GenericNode(sentFront, item, sentFront.next);
+		sentFront.next.next.prev = sentFront.next;
+		size += 1;
+	}
+
+	/**  Adds an item of type T to the back of the deque.*/
+	public void addLast(T item) {
+		sentBack.prev = new GenericNode(sentBack.prev, item, sentBack);
+		sentBack.prev.prev.next = sentBack.prev;
+		size += 1;
+	}
+
+	/**  Returns true if deque is empty, false otherwise.*/
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	/**  Returns the number of items in the deque.*/
+	public int size() {
+		return this.size;
+	}
+ 
+	/**  Prints the items in the deque from first to last, 
+		separated by a space. Once all the items have been 
+		printed, print out a new line.*/
+	public void printDeque() {
+		GenericNode temp = sentFront.next;
+		while (temp != sentBack) {
+			System.out.print(temp.item + " ");
+			temp = temp.next;
+		}
+		System.out.println();
+	}
+
+	/**  Removes and returns the item at the front of the deque. 
+		 If no such item exists, returns null.*/
+	public T removeFirst() {
+		if (size == 0) {
+			return null;
+		} else {
+			T temp = sentFront.next.item;
+			sentFront.next = sentFront.next.next;
+			sentFront.next.prev = sentFront;
+			this.size -= 1;
+			return temp;
+		}
+	}
+
+	/**  Removes and returns the item at the back of the deque. 
+		 If no such item exists, returns null.*/
+	public T removeLast() {
+		if (size == 0) {
+			return null;
+		} else {
+			T temp = sentBack.prev.item;
+			sentBack.prev = sentBack.prev.prev;
+			sentBack.prev.next = sentBack;
+			this.size -= 1;
+			return temp;
+		}
+	}
+
+	/**  Gets the item at the given index, where 0 is the front, 
+		1 is the next item, and so forth. If no such item exists, 
+		returns null. Must not alter the deque!*/
+	public T get(int index) {
+		if (index < 0 || index >= this.size) {
+			return null;
+		} else {
+			GenericNode temp = this.sentFront.next; 
+			for (int i = 0; i < index; i++) {
+				temp = temp.next;
+			}
+			return temp.item;
+		}
+	}
+
+	/* Same as get, but uses recursion. */
+	public T getRecursive(int index) {
+		if (index < 0 || index >= this.size) {
+			return null;
+		}
+		GenericNode temp = this.sentFront.next;
+		return temp.traverseGenericNode(index);
+	}
+
+//	public static void main(String[] args) {
+//		LinkedListDeque<String> test1 = new LinkedListDeque<>();
+//		test1.addFirst("One");
+//		// test1.size();
+//		// test1.removeLast();
+//		// test1.size();
+//		test1.addLast("Two");
+//		test1.printDeque();
+//		test1.removeLast();
+//		// System.out.println(test1.getRecursive(2));
+//		// System.out.println("---------------");
+//		test1.printDeque();
+//		// test1.size();
+//		// test1.removeFirst();
+//		// test1.size();
+//		LinkedListDeque<String> test2 = new LinkedListDeque<>(test1);
+//		test2.printDeque();
+//		test1.printDeque();
+//	}
+}
--- proj1a/LinkedListDequeTest.java
+++ proj1a/LinkedListDequeTest.java
@@ -35,14 +35,12 @@ public class LinkedListDequeTest {
 	  * && is the "and" operation. */
 	public static void addIsEmptySizeTest() {
 		System.out.println("Running add/isEmpty/Size test.");
-		System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-		/*
 		LinkedListDeque<String> lld1 = new LinkedListDeque<String>();
 
 		boolean passed = checkEmpty(true, lld1.isEmpty());
 
 		lld1.addFirst("front");
-		
+
 		// The && operator is the same as "and" in Python.
 		// It's a binary operator that returns true if both arguments true, and false otherwise.
 		passed = checkSize(1, lld1.size()) && passed;
@@ -58,7 +56,6 @@ public class LinkedListDequeTest {
 		lld1.printDeque();
 
 		printTestStatus(passed);
-		*/
 	}
 
 	/** Adds an item, then removes an item, and ensures that dll is empty afterwards. */
@@ -66,8 +63,6 @@ public class LinkedListDequeTest {
 
 		System.out.println("Running add/remove test.");
 
-		System.out.println("Make sure to uncomment the lines below (and delete this print statement).");
-		/*
 		LinkedListDeque<Integer> lld1 = new LinkedListDeque<Integer>();
 		// should be empty 
 		boolean passed = checkEmpty(true, lld1.isEmpty());
@@ -81,7 +76,6 @@ public class LinkedListDequeTest {
 		passed = checkEmpty(true, lld1.isEmpty()) && passed;
 
 		printTestStatus(passed);
-		*/
 	}
 
 	public static void main(String[] args) {
