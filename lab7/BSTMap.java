f94e04da2b0d8518ffe2af69892865117a41a9bf
r-k-jonynas <e0191908@u.nus.edu>
2019-06-04 21:45:06 +0300

    Add remove for TreeMap (Lab 7)

--- lab7/BSTMap.java
+++ lab7/BSTMap.java
@@ -1,13 +1,12 @@
+import edu.princeton.cs.algs4.Stack;
+
+import java.util.HashSet;
+import java.util.InputMismatchException;
 import java.util.Iterator;
 import java.util.Set;
 
 public class BSTMap<K extends Comparable, V> implements Map61B<K, V> {
     private Node root;
-
-    @Override
-    public Iterator<K> iterator() {
-        throw new UnsupportedOperationException();
-    }
 
     private class Node {
         private K key;
@@ -18,25 +17,34 @@ public class BSTMap<K extends Comparable, V> implements Map61B<K, V> {
         private Node(K key, V val) {
             this.key = key;
             this.val = val;
-            // Not sure about the next line.
             this.size = 1;
         }
     }
+
+    /**
+     * Wipe out all the items from BSTMap.
+     */
     @Override
     public void clear() {
         this.root = null;
     }
 
+    /** Check if the BSTMap contains the input key
+     */
     @Override
     public boolean containsKey(K key) {
         return get(key) != null;
     }
 
+    /** Return the value for the input key from this BSTMap
+     */
     @Override
     public V get(K key) {
         return get(root, key);
     }
 
+    /** Helper for get
+     */
     private V get(Node node, K key) {
         if (node == null) {
             return null;
@@ -52,12 +60,18 @@ public class BSTMap<K extends Comparable, V> implements Map61B<K, V> {
         }
     }
 
+    /**
+     * @return the size of the BSTMap
+     */
     @Override
     public int size() {
         return size(root);
     }
 
-    // TODO: Fix this
+    /**
+     * Helper for size()
+     * @return the size of the input node
+     */
     private int size(Node node) {
         if (node == null) {
             return 0;
@@ -65,27 +79,111 @@ public class BSTMap<K extends Comparable, V> implements Map61B<K, V> {
         return size(node.left) + size(node.right) + node.size;
     }
 
-
+    /**
+     * Returns a Set view of the keys contained in this map.
+     */
     @Override
     public Set<K> keySet() {
-        throw new UnsupportedOperationException();
+        Set<K> keys = new HashSet<>();
+        keyAggregation(root, keys);
+        return keys;
     }
 
+    void keyAggregation(Node n, Set<K> set) {
+        if (n != null) {
+            keyAggregation(n.left, set);
+            set.add(n.key);
+            keyAggregation(n.right, set);
+        }
+    }
+
+    /**
+     * Delete the key-value pair (key, value) and return value.
+     * @param key
+     * @return null if key not in BSTMap. Otherwise, V value.
+     */
     @Override
     public V remove(K key) {
-        throw new UnsupportedOperationException();
+        V value = get(key);
+        root = remove(root, key);
+        return value;
     }
 
+    /**
+     * Helper for remove
+     */
+    private Node remove(Node root, K key) {
+        int cmp = key.compareTo(root.key);
+        if (cmp < 0) {
+            root.left = remove(root.left, key);
+            return root;
+        }
+        if (cmp > 0) {
+            root.right = remove(root.right, key);
+            return root;
+        } else {
+            /* - Deletion key has no children  */
+            if (root.left == null && root.right == null) {
+                root = null;
+                return root;
+            /* - Deletion key has one child  */
+            } else if (root.left == null && root.right != null) {
+                root = root.right;
+                return root;
+            } else if (root.left != null && root.right == null) {
+                root = root.left;
+                return root;
+            /*- Deletion key has two children */
+            } else {
+                Node successor = findMin(root.right);
+                root.key = successor.key;
+                root.val = successor.val;
+                root.right = remove(root.right, root.key);
+                return root;
+            }
+        }
+    }
+    /**
+     * Alternative remove function. Removes key/value pair.
+     * If such a pair isn't found, throws exception.
+     */
     @Override
     public V remove(K key, V value) {
-        throw new UnsupportedOperationException();
+        V valFromMap = get(key);
+        if (value.equals(valFromMap)) {
+            root = remove(root, key);
+            return value;
+        } else {
+            throw new InputMismatchException();
+        }
     }
 
+    /**
+     * Returns the node with the smallest key within the input subtree.
+    */
+    private Node findMin(Node node) {
+        Node temp = node;
+        while (temp.left != null) {
+            temp = temp.left;
+        }
+        return temp;
+    }
+
+    /**
+     * Puts a key/value pair into BSTMap
+     */
     @Override
     public void put(K key, V value) {
         root = put(root, key, value);
     }
 
+    /**
+     * Traverses a node to put a key/value pair into the BSTMap subtree
+     * @param n node to be traversed
+     * @param key input key
+     * @param value input value
+     * @return
+     */
     private Node put (Node n, K key, V value) {
         if (n == null) {
             return new Node(key, value);
@@ -102,11 +200,16 @@ public class BSTMap<K extends Comparable, V> implements Map61B<K, V> {
         return n;
     }
 
-
+    /**
+     * Prints inorder traversal of BSTMap
+     */
     public void printInOrder() {
         printNode(root);
     }
 
+    /**
+     * Helper for inorder print
+     */
     private void printNode(Node n) {
         if (n == null) {
             return;
@@ -115,6 +218,43 @@ public class BSTMap<K extends Comparable, V> implements Map61B<K, V> {
             System.out.print(n.key + ", " + n.val);
             System.out.print(" ");
             printNode(n.right);
+        }
+    }
+
+    /**
+     * Custom BSTMap iterator
+     */
+    @Override
+    public Iterator<K> iterator() {
+        return new BSTMapIterator();
+    }
+
+    private class BSTMapIterator implements Iterator<K> {
+        Stack<Node> stack = new Stack<Node>();
+
+        private BSTMapIterator() {
+            pushToLeft(root);
+        }
+
+        private void pushToLeft(Node node) {
+            if(node != null) {
+                stack.push(node);
+                pushToLeft(node.left);
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            if(stack.isEmpty()) {
+                return false;
+            } return true;
+        }
+
+        @Override
+        public K next() {
+            Node temp = stack.pop();
+            pushToLeft(temp.right);
+            return temp.key;
         }
     }
 
--- lab7/TestBSTMap.java
+++ lab7/TestBSTMap.java
@@ -1,8 +1,162 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+import java.util.Iterator;
+
 /** Tests by Brendan Hu, Spring 2015, revised for 2016 by Josh Hug */
 public class TestBSTMap {
+
+    @Test
+    public void iteratorTest() {
+        try {
+            BSTMap<Integer, Integer> a = new BSTMap<>();
+            for (int i = 0; i < 455; i++) {
+                a.put(i, 1+i);
+            }
+            Iterator<Integer> seer = a.iterator();
+            while (seer.hasNext()) {
+                System.out.println(seer.next());
+            }
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    /* MY TESTS @RKJ*/
+
+    @Test
+    public void testRemove1() {
+        BSTMap<Integer,String> q = new BSTMap<>();
+        q.put(5,"a");
+        q.put(2,"b");
+        q.put(1,"c");
+        q.put(4,"d");
+        q.put(6,"e");
+
+        assertEquals("e", q.remove(6));
+        assertTrue(q.containsKey(1));
+        assertTrue(q.containsKey(2));
+        assertTrue(q.containsKey(4));
+        assertTrue(q.containsKey(5));
+        assertEquals(false, q.containsKey(6));
+
+
+        assertEquals("d", q.remove(4));
+        assertTrue(q.containsKey(1));
+        assertTrue(q.containsKey(2));
+        assertTrue(q.containsKey(5));
+        assertEquals(false, q.containsKey(6));
+        assertEquals(false, q.containsKey(4));
+    }
+
+
+    @Test
+    public void testRemove2() {
+        BSTMap<Integer,String> q = new BSTMap<>();
+        q.put(5,"a");
+        q.put(2,"b");
+        q.put(1,"c");
+        q.put(4,"d");
+        q.put(6,"e");
+        q.put(7,"f");
+        q.put(3,"z");
+
+        assertEquals("e", q.remove(6));
+        assertTrue(q.containsKey(1));
+        assertTrue(q.containsKey(2));
+        assertTrue(q.containsKey(3));
+        assertTrue(q.containsKey(4));
+        assertTrue(q.containsKey(5));
+        assertTrue(q.containsKey(7));
+        assertFalse(q.containsKey(6));
+
+        assertEquals("d", q.remove(4));
+        assertTrue(q.containsKey(1));
+        assertTrue(q.containsKey(2));
+        assertTrue(q.containsKey(3));
+        assertTrue(q.containsKey(5));
+        assertTrue(q.containsKey(7));
+        assertFalse(q.containsKey(6));
+        assertFalse(q.containsKey(4));
+    }
+
+    @Test
+    public void testRemove3() {
+        BSTMap<Integer,String> q = new BSTMap<>();
+        q.put(5,"a");
+        q.put(2,"b");
+        q.put(1,"c");
+        q.put(4,"d");
+        q.put(6,"e");
+        q.put(7,"f");
+        q.put(3,"z");
+
+        assertEquals("b", q.remove(2));
+        assertTrue(q.containsKey(1));
+        assertTrue(q.containsKey(6));
+        assertTrue(q.containsKey(3));
+        assertTrue(q.containsKey(4));
+        assertTrue(q.containsKey(5));
+        assertTrue(q.containsKey(7));
+        // Make sure that it is 1 3 (subroot) 2
+        assertFalse(q.containsKey(2));
+
+        assertEquals("z", q.remove(3));
+        assertTrue(q.containsKey(1));
+        assertTrue(q.containsKey(4));
+        assertTrue(q.containsKey(6));
+        assertTrue(q.containsKey(5));
+        assertTrue(q.containsKey(7));
+        assertFalse(q.containsKey(3));
+        assertFalse(q.containsKey(2));
+    }
+
+    @Test
+    public void testRemoveRoot() {
+        BSTMap<Integer,String> q = new BSTMap<>();
+        q.put(5,"a");
+        q.put(2,"b");
+        q.put(1,"c");
+        q.put(4,"d");
+        q.put(6,"e");
+        q.put(7,"f");
+        q.put(3,"z");
+
+        assertEquals("a", q.remove(5));
+        assertTrue(q.containsKey(1));
+        assertTrue(q.containsKey(2));
+        assertTrue(q.containsKey(3));
+        assertTrue(q.containsKey(4));
+        assertTrue(q.containsKey(6));
+        assertTrue(q.containsKey(7));
+        assertFalse(q.containsKey(5));
+    }
+
+    @Test
+    public void testRemoveThreeCases() {
+        BSTMap<String,String> q = new BSTMap<String,String>();
+        q.put("c","a");
+        q.put("b","a");
+        q.put("a","a");
+        q.put("d","a");
+        q.put("e","a");                         // a b c d e
+        assertEquals("a", q.remove("e"));      // a b c d
+        assertTrue(q.containsKey("a"));
+        assertTrue(q.containsKey("b"));
+        assertTrue(q.containsKey("c"));
+        assertTrue(q.containsKey("d"));
+        assertEquals("a", q.remove("c"));      // a b d
+        assertTrue(q.containsKey("a"));
+        assertTrue(q.containsKey("b"));
+        assertTrue(q.containsKey("d"));
+        q.put("f","a");                         // a b d f
+        assertTrue(null != q.remove("d"));      // a b f
+        assertTrue(q.containsKey("a"));
+        assertTrue(q.containsKey("b"));
+        assertTrue(q.containsKey("f"));
+    }
+
+    /* MY OWN TESTS END @RKJ*/
 
 	@Test
     public void sanityGenericsTest() {
